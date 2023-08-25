f945674e3767462ec98b3e5c855f82fe56c7fd15
ovibaridar <ovibaridar06@gmail.com>
2023-08-25 21:52:23 +0600

    series

--- series.py
+++ series.py
@@ -1,23 +1,32 @@
 # series  is like  1D array
 import pandas as pd
 import numpy as nm
+
 # list to series pandas
 data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 5, 4, 2, 2, 4154, 4, 1524, 424, 24, 24, 24, 24, 242, 42, 424, 24, 24, 24, 24, 2,
         4242]
-st = pd.Series(data)
+st = pd.Series(data, name='New set')
 print("new series list = \n", st)
 
 # Numpy to series pandas
-data = nm.arange(1, 20000, 5)
-st = pd.Series(data)
-print("new series Numpy = \n", st)
-
+# data = nm.arange(1, 20000, 5)
+# st = pd.Series(data)
+# print("new series Numpy = \n", st)
 # Dictionary  to series pandas
+# data = {
+#     'Id': [1, 2 ],
+#     'Name': ["Al Arman Ovi", "Al Arman Ovi"],
+# }
+#
+# st = pd.Series(data)
+# print("new series Dictionary = \n", st)
 
-data = {
-    'Id': [1, 2 ],
-    'Name': ["Al Arman Ovi", "Al Arman Ovi"],
-}
+print("only values >>>>>>>>>>>> \n", st.values)
+print("name >>>> \n", st.name)
+print("name >>>> \n", st.name)
+print("byte >>>> \n", st.nbytes)
+print("dtype >>>> \n", st.dtype)
+print("Size >>>> \n", st.size)
+print("Shape >>>> \n", st.shape)
 
-st = pd.Series(data)
-print("new series Dictionary = \n", st)
+
