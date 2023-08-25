a1f5999877a0451dda1c4b2ebbd3003a92b0c6e5
ovibaridar <ovibaridar06@gmail.com>
2023-08-25 22:20:31 +0600

    using DataFrame csv file import and use

--- DataFrame.py
+++ DataFrame.py
@@ -3,10 +3,20 @@ import pandas as pd
 path = "I:\Pandas\pandas\csv\BDUNI.csv"
 pds = pd.read_csv(path)
 
-for index, row in pds.iterrows():
-    university = row["University"]
-    phd_granting = row["Ph.D. granting"]
 
-    # Check if "Ph.D. granting" is "Yes" or "No" and print accordingly
-    if phd_granting == "Yes":
-        print(f"{university} --------->> {phd_granting}")
+# for index, row in pds.iterrows():
+#     university = row["University"]
+#     phd_granting = row["Ph.D. granting"]
+#
+#     # Check if "Ph.D. granting" is "Yes" or "No" and print accordingly
+#     if phd_granting == "Yes":
+#         print(f"{university} --------->> {phd_granting}")
+
+
+# using DataFrame
+sr = pd.DataFrame(pds)
+for index, row in sr.iterrows():
+    university = row["University"]
+    granting = row["Ph.D. granting"]
+    if granting == "Yes":
+        print(university, "----------------->>>>>>>>>>>", granting)
