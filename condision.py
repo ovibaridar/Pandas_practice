74749971c9b8f2b47ca966ada43c5827157383e4
ovibaridar <ovibaridar06@gmail.com>
2023-08-30 00:16:47 +0600

    condition

---
+++ condision.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+input_path = "I:/Pandas/pandas/csv/Salary.csv"
+
+# Read the CSV file into a DataFrame
+pds = pd.read_csv(input_path)
+
+print("Before condition \n", pds)
+pds.drop_duplicates(inplace=True)
+print("after condition with and \n", pds.loc[(pds['Total'] > 100000) & (pds['Salary'] > 101301)])
+
+print("after condition with or  \n", pds.loc[(pds['Total'] > 100000) | (pds['Salary'] > 101302.0)])
+
+print("after condition not \n", pds.loc[~((pds['Total'] > 100000) & (pds['Salary'] > 101301))])
