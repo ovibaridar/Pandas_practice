1934de80f91d0493abc604c016b8f074c41444a7
ovibaridar <ovibaridar06@gmail.com>
2023-09-14 00:41:55 +0600

    Group by  and  agg function

---
+++ Grupby and agg.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+product = [
+    ("Apple", 20, "Bogura"),
+    ("Apple", 25, "Narayanganj"),
+    ("Apple", 24, "Joypurhat"),
+    ("Orange", 30, "Noakhali"),
+    ("Orange", 29, "Bogura"),
+    ("Orange", 25, "Narayanganj"),
+    ("Orange", 24, "Joypurhat"),
+    ("Orange", 30, "Noakhali"),
+    ("Mango", 30, "Bogura"),
+    ("Mango", 25, "Narayanganj"),
+    ("Mango", 24, "Joypurhat"),
+    ("Mango", 30, "Noakhali"),
+    ("Banana", 45, "Bogura"),
+    ("Banana", 25, "Narayanganj"),
+    ("Banana", 24, "Joypurhat"),
+    ("Banana", 30, "Noakhali")
+]
+
+Dataset = pd.DataFrame(product, columns=["Name", "Price", "District"])
+Dataset2 = Dataset.groupby("Name")
+print(Dataset2[["Price", "District"]].agg(["min", "max","count"]))
